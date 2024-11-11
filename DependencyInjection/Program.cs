920149abda0c0942eac135a539ef877e031cf04c
Ömer Faruk CAVLAK <omerfarukcavlak@gmail.com>
2024-11-11 20:03:42 +0300

    Dependency Injection Design Pattern Added

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,59 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IKernel kernel = new StandardKernel();
+            kernel.Bind<IProductDal>().To<EfProductDal>().InSingletonScope();
+            ProductManager productManager = new ProductManager(kernel.Get<IProductDal>());
+            
+            productManager.Save();
+            Console.ReadLine();
+        }
+    }
+
+    interface IProductDal
+    {
+        void Save();
+    }
+
+    class EfProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with EF");
+        }
+
+    }
+    class NhProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Nh");
+        }
+
+    }
+
+    class ProductManager
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Save()
+        {
+            _productDal.Save();
+        }
+    }
+}
